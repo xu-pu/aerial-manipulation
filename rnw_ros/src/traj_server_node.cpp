a6c86ce3e3085a8a251aff01c905ed466f35411c
plutotx <ptx.pluto@gmail.com>
2020-07-05 23:44:15 +0800

    traj server node added

---
+++ rnw_ros/src/traj_server_node.cpp
@@ -0,0 +1,79 @@
+#include "rnw_ros/traj_uitls.h"
+
+PolynomialTraj traj;
+
+ros::Publisher pub_path_setpoint;
+
+void pub_setpoint(){
+
+  static ros::Time init_time;
+  static bool init = false;
+
+  static nav_msgs::Path path;
+  path.header.frame_id = "world";
+
+  if (!init) {
+    init_time = ros::Time::now();
+    init = true;
+  }
+
+  auto cur_time = ros::Time::now();
+  double dt = ( cur_time - init_time ).toSec();
+
+  if ( dt < traj.getTimeSum() ) {
+
+    Vector3d x = traj.evaluate(dt);
+    Vector3d x_dot = traj.evaluateVel(dt);
+    Vector3d x_dot_dot = traj.evaluateAcc(dt);
+
+    ROS_INFO_STREAM(dt);
+    ROS_INFO_STREAM(x);
+
+    geometry_msgs::PoseStamped pose;
+    pose.header.stamp = cur_time;
+    pose.pose.position = eigen2ros(x);
+
+    path.poses.push_back(pose);
+
+    pub_path_setpoint.publish(path);
+
+  }
+  else {
+    ROS_INFO_STREAM("Traj Complete");
+  }
+
+}
+
+int main( int argc, char** argv ) {
+
+  ros::init(argc,argv,"pub_frames_node");
+
+  ros::NodeHandle nh("~");
+
+  pub_path_setpoint = nh.advertise<nav_msgs::Path>("/traj/setpoint",10);
+
+  vector<Vector3d> waypoints = gen_waypoint_zigzag(5,0.25,0.5);
+
+  MatrixXd POS = toXd(waypoints);
+  VectorXd TIMES = gen_time_intervals(1,waypoints);
+  PolynomialTraj test_traj = minSnapTraj(POS,Vector3d::Zero(),Vector3d::Zero(),Vector3d::Zero(),Vector3d::Zero(),TIMES);
+
+  traj = test_traj;
+
+  ros::Rate rate(100);
+
+  while (ros::ok()) {
+
+    pub_setpoint();
+
+    ros::spinOnce();
+
+    rate.sleep();
+
+  }
+
+  ros::shutdown();
+
+  return 0;
+
+}
