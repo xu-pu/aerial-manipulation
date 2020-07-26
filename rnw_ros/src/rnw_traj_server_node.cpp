545b5a7c3eaed7692acfe513a29698c9d82330b3
plutotx <ptx.pluto@gmail.com>
2020-07-27 04:43:16 +0800

    add full logic

--- rnw_ros/src/rnw_traj_server_node.cpp
+++ rnw_ros/src/rnw_traj_server_node.cpp
@@ -143,6 +143,54 @@ struct traj_server_t {
 
     }
 
+    void on_trigger_insert(){
+      on_cleanup();
+      Matrix3d R = pose2R(cur_uav_odom.pose.pose);
+      Vector3d T = pose2T(cur_uav_odom.pose.pose);
+      Vector3d pt1(0,0,0);
+      Vector3d pt2(0,0,-0.03);
+      Vector3d pt3(0,0,-0.06);
+      vector<Vector3d> waypoints = transform_pts({pt1,pt2,pt3},R,T);
+      traj = minsnap(waypoints,1);
+      traj_available = true;
+      base_yaw = odom2yaw(cur_uav_odom);
+    }
+
+    void on_trigger_rock(){
+
+      constexpr double topple_dist = 0.06;
+      constexpr double rock_dist = 0.1;
+      constexpr size_t cycles = 3;
+
+      Matrix3d R = pose2R(cur_uav_odom.pose.pose);
+      Vector3d T = pose2T(cur_uav_odom.pose.pose);
+
+      vector<Vector3d> pts;
+      pts.emplace_back(0,0,0);
+      pts.emplace_back(topple_dist,0,0);
+      for ( size_t i=0; i<cycles; i++ ) {
+        pts.emplace_back(topple_dist,rock_dist,0);
+        pts.emplace_back(topple_dist,-rock_dist,0);
+      }
+      pts.emplace_back(topple_dist,0,0);
+
+      vector<Vector3d> waypoints = transform_pts(pts, R, T);
+      traj = minsnap(waypoints,2);
+      traj_available = true;
+      base_yaw = quat2eulers(Quaterniond(R)).z();
+
+    }
+
+    void on_trigger_rock_walk(){
+      Matrix3d R = pose2R(cur_uav_odom.pose.pose);
+      Vector3d T = pose2T(cur_uav_odom.pose.pose);
+      base_yaw = quat2eulers(Quaterniond(R)).z();
+      vector<Vector3d> waypoints = gen_waypoint_zigzag(5,0.25,0.5);
+      vector<Vector3d> wps = transform_pts(waypoints,R,T);
+      traj = minsnap(wps,3);
+      traj_available = true;
+    }
+
     void on_trigger_go_to_tip(){
 
       on_cleanup();
@@ -184,9 +232,24 @@ void on_trigger( geometry_msgs::PoseStampedConstPtr const & msg ){
   traj_server.on_trigger(msg);
 }
 
-void on_trigger_tip(std_msgs::HeaderConstPtr const & msg ){
+void on_trigger_go_to_tip(std_msgs::HeaderConstPtr const & msg ){
   ROS_INFO_STREAM("Trigger: Go to the tip");
   traj_server.on_trigger_go_to_tip();
+}
+
+void on_trigger_insert(std_msgs::HeaderConstPtr const & msg ){
+  ROS_INFO_STREAM("Trigger: Insert");
+  traj_server.on_trigger_insert();
+}
+
+void on_trigger_rock(std_msgs::HeaderConstPtr const & msg ){
+  ROS_INFO_STREAM("Trigger: Rock");
+  traj_server.on_trigger_rock();
+}
+
+void on_trigger_rock_walk(std_msgs::HeaderConstPtr const & msg ){
+  ROS_INFO_STREAM("Trigger: Rock Walk");
+  traj_server.on_trigger_rock_walk();
 }
 
 void on_tip( geometry_msgs::PointStampedConstPtr const & msg ){
@@ -206,7 +269,11 @@ int main( int argc, char** argv ) {
   ros::Subscriber sub_odom = nh.subscribe<nav_msgs::Odometry>("/uwb_vicon_odom",10,on_odom);
   ros::Subscriber sub_trigger = nh.subscribe<geometry_msgs::PoseStamped>("/traj_start_trigger",10,on_trigger);
   ros::Subscriber sub_tip = nh.subscribe<geometry_msgs::PointStamped>("/cone/tip",1,on_tip);
-  ros::Subscriber sub_trigger_tip = nh.subscribe<std_msgs::Header>("/rnw/trigger/go_to_tip", 10, on_trigger_tip);
+
+  ros::Subscriber sub_trigger_tip = nh.subscribe<std_msgs::Header>("/rnw/trigger/go_to_tip", 10, on_trigger_go_to_tip);
+  ros::Subscriber sub_trigger_insert = nh.subscribe<std_msgs::Header>("/rnw/trigger/insert", 10, on_trigger_insert);
+  ros::Subscriber sub_trigger_rock = nh.subscribe<std_msgs::Header>("/rnw/trigger/rock", 10, on_trigger_rock);
+  ros::Subscriber sub_trigger_rock_walk = nh.subscribe<std_msgs::Header>("/rnw/trigger/rock_walk", 10, on_trigger_rock_walk);
 
   ros::spin();
 
