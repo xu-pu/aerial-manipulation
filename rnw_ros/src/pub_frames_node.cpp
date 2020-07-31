cfce3e05e40d20143196342fbe8b7258efd99153
plutotx <ptx.pluto@gmail.com>
2020-08-01 02:24:40 +0800

    remove euler angles from pub_frames

--- rnw_ros/src/pub_frames_node.cpp
+++ rnw_ros/src/pub_frames_node.cpp
@@ -11,8 +11,6 @@
 #include "rnw_ros/pose_utils.h"
 #include "rnw_ros/traj_uitls.h"
 
-ros::Publisher pub_rpy_imu;
-ros::Publisher pub_rpy_odom;
 ros::Publisher pub_cone_tip;
 ros::Publisher pub_cage_tcp;
 Vector3d last_t;
@@ -36,13 +34,7 @@ Matrix3d calc_intermediate_R( double yaw ){
 
 
 void on_odom( OdometryConstPtr const & odom ){
-  Eigen::Matrix3d R_FLU2FRD;
-  R_FLU2FRD << 1, 0, 0, 0, -1, 0, 0, 0, -1;
-  Eigen::Matrix3d R_ENU2NED;
-  R_ENU2NED << 0, 1, 0, 1, 0, 0, 0, 0, -1;
   Vector3d rpy = odom2rpy(odom);
-  pub_rpy_odom.publish(eigen2ros(rpy));
-
   Matrix3d R = odom2R(odom);
   Vector3d T = odom2T(odom);
   publish_frame(R,T,"uav_odom","world");
@@ -75,15 +67,11 @@ void on_cone( OdometryConstPtr const & odom ){
 }
 
 void on_vins( OdometryConstPtr const & odom ){
-  Vector3d rpy = odom2rpy(odom);
-  pub_rpy_odom.publish(eigen2ros(rpy));
   publish_frame(odom2R(odom),odom2T(odom),"vins","world");
   last_t = odom2T(odom);
 }
 
 void on_imu( sensor_msgs::ImuConstPtr const & imu ){
-  Vector3d rpy = imu2rpy(imu);
-  pub_rpy_imu.publish(eigen2ros(rpy));
   Matrix3d R = imu2R(imu);
   publish_frame(R,last_t,"imu","world");
 }
@@ -107,8 +95,6 @@ int main( int argc, char** argv ) {
   ros::Subscriber sub_cone = nh.subscribe<nav_msgs::Odometry>("cone",10,on_cone);
   ros::Subscriber sub_vins = nh.subscribe<nav_msgs::Odometry>("vins",10,on_vins);
 
-  pub_rpy_imu = nh.advertise<geometry_msgs::Vector3>("/uav/rpy_imu",1);
-  pub_rpy_odom = nh.advertise<geometry_msgs::Vector3>("/uav/rpy_odom",1);
   pub_cone_tip = nh.advertise<geometry_msgs::PointStamped>("/cone/tip",1);
   pub_cage_tcp = nh.advertise<geometry_msgs::PointStamped>("/uav/tcp",1);
 
