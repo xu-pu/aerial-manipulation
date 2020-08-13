8d5f4e216dbd80e19bbc9fba97b526053062b0ec
plutotx <ptx.pluto@gmail.com>
2020-08-13 21:24:25 +0800

    plan for insert and goto tip

--- rnw_ros/src/rnw_controller_node.cpp
+++ rnw_ros/src/rnw_controller_node.cpp
@@ -3,6 +3,9 @@
 #include <quadrotor_msgs/PolynomialTrajectory.h>
 #include <nav_msgs/Odometry.h>
 
+#include "rnw_ros/rnw_utils.h"
+#include "rnw_ros/pose_utils.h"
+
 #include "am_traj/am_traj.hpp"
 #include "am_traj/ros_msgs.h"
 
@@ -14,8 +17,15 @@ struct rnw_controller_t {
 
     geometry_msgs::PointStamped latest_cone_tip;
 
-    explicit rnw_controller_t(ros::NodeHandle & nh){
+    rnw_config_t rnw_config;
+
+    AmTraj traj_generator;
+
+    explicit rnw_controller_t(ros::NodeHandle & nh)
+            : traj_generator(1024, 16, 0.4, 1, 1, 23, 0.02)
+    {
       pub_poly_traj = nh.advertise<quadrotor_msgs::PolynomialTrajectory>("poly_traj",10,true);
+      rnw_config.load_from_ros(nh);
     }
 
     void on_uav_odom( nav_msgs::OdometryConstPtr const & msg ){
@@ -28,9 +38,42 @@ struct rnw_controller_t {
 
     void on_trigger_n3ctrl( geometry_msgs::PoseStampedConstPtr const & msg ){}
 
-    void on_trigger_insert( std_msgs::HeaderConstPtr const & msg ){}
+    void on_trigger_insert( std_msgs::HeaderConstPtr const & msg ){
 
-    void on_trigger_go_to_tip( std_msgs::HeaderConstPtr const & msg ){}
+      Vector3d pt_uav = pose2T(latest_uav_odom.pose.pose);
+      Vector3d pt_tip(latest_cone_tip.point.x, latest_cone_tip.point.y, latest_cone_tip.point.z);
+
+      double z_planned_tcp = pt_tip.z() + rnw_config.hover_above_tip;
+      double z_planned_uav = z_planned_tcp - rnw_config.X_tcp_cage.z(); // offset between imu and tcp
+
+      Vector3d pt_tgt = pt_tip;
+      pt_tgt.z() = z_planned_uav;
+
+      Vector3d pt_inserted = pt_tgt;
+      pt_inserted.z()  = pt_inserted.z() - rnw_config.hover_above_tip - rnw_config.insert_below_tip;
+
+      Vector3d v0 = Vector3d::Zero();
+      Trajectory traj = traj_generator.genOptimalTrajDTC({pt_uav, pt_tgt, pt_inserted}, v0, v0, v0, v0);
+      pub_poly_traj.publish(to_ros_msg(traj,ros::Time::now()));
+
+    }
+
+    void on_trigger_go_to_tip( std_msgs::HeaderConstPtr const & msg ){
+
+      Vector3d pt_uav = pose2T(latest_uav_odom.pose.pose);
+      Vector3d pt_tip(latest_cone_tip.point.x, latest_cone_tip.point.y, latest_cone_tip.point.z);
+
+      double z_planned_tcp = pt_tip.z() + rnw_config.hover_above_tip;
+      double z_planned_uav = z_planned_tcp - rnw_config.X_tcp_cage.z(); // offset between imu and tcp
+
+      Vector3d pt_tgt = pt_tip;
+      pt_tgt.z() = z_planned_uav;
+
+      Vector3d v0 = Vector3d::Zero();
+      Trajectory traj = traj_generator.genOptimalTrajDTC({pt_uav, pt_tgt}, v0, v0, v0, v0);
+      pub_poly_traj.publish(to_ros_msg(traj,ros::Time::now()));
+
+    }
 
     void on_trigger_rock( std_msgs::HeaderConstPtr const & msg ){}
 
