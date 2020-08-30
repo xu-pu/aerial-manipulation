6270fa68591d9505a457b73c42fffe22ecbb1203
plutotx <ptx.pluto@gmail.com>
2020-08-30 22:13:58 +0800

    Revert "onboard calib"
    This reverts commit 222c146af6b4c1337363a99c0469ec2831b9b3c0.

--- rnw_ros/src/vicon_imu_cali_node.cpp
+++ rnw_ros/src/vicon_imu_cali_node.cpp
@@ -17,6 +17,53 @@
 using sensor_msgs::ImuConstPtr;
 using nav_msgs::OdometryConstPtr;
 
+struct cali_sampler_t {
+
+    vector<Vector3d> samples_rpy;
+    vector<sensor_msgs::Imu> samples_imu;
+    vector<nav_msgs::Odometry> samples_odom;
+
+    static constexpr size_t sample_threshold = 500;
+    static constexpr double deg_threshold = 1;
+
+    bool check_rpy( Vector3d const & rpy ){
+
+      for ( auto const & pt : samples_rpy ) {
+
+        double dist = (rpy-pt).norm();
+
+        if ( dist < deg2rad(deg_threshold) ) {
+          return false;
+        }
+
+      }
+
+      return true;
+
+    }
+
+    void sample( ImuConstPtr const & imu, OdometryConstPtr const & odom ){
+
+      Vector3d rpy = imu2rpy(imu);
+
+      if ( check_rpy(rpy) ) {
+        samples_rpy.push_back(rpy);
+        samples_imu.push_back(*imu);
+        samples_odom.push_back(*odom);
+        ROS_INFO_STREAM("Samples Collected: " << samples_rpy.size() << "/" << sample_threshold);
+      }
+
+    }
+
+    bool enough_samples() const {
+      return samples_rpy.size() > sample_threshold;
+    }
+
+};
+
+cali_sampler_t cali_sampler;
+
+
 struct ViconImuAlignError {
 
     Quaterniond R_VICON_MARKER;
@@ -51,134 +98,72 @@ struct ViconImuAlignError {
 
 };
 
-struct cali_sampler_t {
+void calibrate_vicon_imu( cali_sampler_t & sample ){
 
-    vector<Vector3d> samples_rpy;
-    vector<sensor_msgs::Imu> samples_imu;
-    vector<nav_msgs::Odometry> samples_odom;
+  using namespace ceres;
 
-    static constexpr size_t sample_threshold = 500;
-    static constexpr double deg_threshold = 1;
+  assert( sample.enough_samples() );
 
-    sensor_msgs::Imu latest_imu;
+  ceres::Problem problem;
 
-    bool init = false;
+  Quaterniond R_MARKER_FLU = Quaterniond::Identity();
 
-    bool check_rpy( Vector3d const & rpy ){
+  double * data = R_MARKER_FLU.coeffs().data();
 
-      for ( auto const & pt : samples_rpy ) {
+  for ( size_t i=0; i<sample.samples_rpy.size(); i++ ) {
 
-        double dist = (rpy-pt).norm();
+    auto const & imu = sample.samples_imu.at(i);
+    auto const & odom = sample.samples_odom.at(i);
 
-        if ( dist < deg2rad(deg_threshold) ) {
-          return false;
-        }
+    ceres::CostFunction * costFunction = new NumericDiffCostFunction<ViconImuAlignError,CENTRAL,2,4>(new ViconImuAlignError(imu,odom));
 
-      }
+    //ceres::LossFunction* lossFunction = new ceres::CauchyLoss(1.0);
+    problem.AddResidualBlock(costFunction,nullptr,data);
 
-      return true;
+  }
 
-    }
+  ceres::LocalParameterization* quaternionParameterization =
+          new ceres::EigenQuaternionParameterization;
 
-    void on_odom( OdometryConstPtr const & msg ){
+  problem.SetParameterization(data,quaternionParameterization);
 
-      if (!init) {
-        ROS_WARN_STREAM("Waiting for IMU...");
-        return;
-      }
+  ceres::Solver::Options options;
+  options.max_num_iterations = 1000;
+  options.num_threads = 1;
+  options.minimizer_progress_to_stdout = true;
 
-      if ( abs((latest_imu.header.stamp - msg->header.stamp).toSec()) > 0.01 ) {
-        // out of sync
-        return;
-      }
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
 
-      if ( !enough_samples() ) {
-        sample(latest_imu,*msg);
-      }
+  std::cout << summary.FullReport() << std::endl;
 
-      if ( enough_samples() ) {
-        ROS_INFO_STREAM("DO CERES CALIBRATION");
-        calibrate();
-        ros::shutdown();
-      }
+  ROS_INFO_STREAM("R_MARKER_FLU: " << R_MARKER_FLU.coeffs().transpose() );
+  ROS_INFO_STREAM("Eigen::Quat: " << R_MARKER_FLU.w() << ", " << R_MARKER_FLU.x() << ", " << R_MARKER_FLU.y() << ", " << R_MARKER_FLU.z());
+  ROS_INFO_STREAM("Magnitude: " << rad2deg(Eigen::AngleAxisd(R_MARKER_FLU).angle()) );
 
-    }
+  cout << "yaml:" << endl;
+  cout << "R_MARKER_FLU:\n"
+          << "   x: " << R_MARKER_FLU.x() << '\n'
+          << "   y: " << R_MARKER_FLU.y() << '\n'
+          << "   z: " << R_MARKER_FLU.z() << '\n'
+          << "   w: " << R_MARKER_FLU.w() << '\n' << endl;
 
-    void sample( sensor_msgs::Imu const & imu, nav_msgs::Odometry const & odom ){
+}
 
-      Vector3d rpy = imu2rpy(imu);
 
-      if ( check_rpy(rpy) ) {
-        samples_rpy.push_back(rpy);
-        samples_imu.push_back(imu);
-        samples_odom.push_back(odom);
-        ROS_INFO_STREAM("Samples Collected: " << samples_rpy.size() << "/" << sample_threshold);
-      }
+void sync_callback( sensor_msgs::ImuConstPtr const & imu, nav_msgs::OdometryConstPtr const & odom ){
 
-    }
+  if ( !cali_sampler.enough_samples() ) {
+    cali_sampler.sample(imu,odom);
+  }
 
-    bool enough_samples() const {
-      return samples_rpy.size() > sample_threshold;
-    }
+  if ( cali_sampler.enough_samples() ) {
+    ROS_INFO_STREAM("DO CERES CALIBRATION");
+    calibrate_vicon_imu(cali_sampler);
+    ros::shutdown();
+  }
 
-    void on_imu( sensor_msgs::ImuConstPtr const & msg ){
-      latest_imu = *msg;
-      init = true;
-    }
-
-    void calibrate(){
-
-      using namespace ceres;
-
-      assert( enough_samples() );
-
-      ceres::Problem problem;
-
-      Quaterniond R_MARKER_FLU = Quaterniond::Identity();
-
-      double * data = R_MARKER_FLU.coeffs().data();
-
-      for ( size_t i=0; i<samples_rpy.size(); i++ ) {
-
-        auto const & imu = samples_imu.at(i);
-        auto const & odom = samples_odom.at(i);
-
-        ceres::CostFunction * costFunction = new NumericDiffCostFunction<ViconImuAlignError,CENTRAL,2,4>(new ViconImuAlignError(imu,odom));
-
-        //ceres::LossFunction* lossFunction = new ceres::CauchyLoss(1.0);
-        problem.AddResidualBlock(costFunction,nullptr,data);
-
-      }
-
-      ceres::LocalParameterization* quaternionParameterization =
-              new ceres::EigenQuaternionParameterization;
-
-      problem.SetParameterization(data,quaternionParameterization);
-
-      ceres::Solver::Options options;
-      options.max_num_iterations = 1000;
-      options.num_threads = 1;
-      options.minimizer_progress_to_stdout = true;
-
-      ceres::Solver::Summary summary;
-      ceres::Solve(options, &problem, &summary);
-
-      std::cout << summary.FullReport() << std::endl;
-
-      ROS_INFO_STREAM("R_MARKER_FLU: " << R_MARKER_FLU.coeffs().transpose() );
-      ROS_INFO_STREAM("Eigen::Quat: " << R_MARKER_FLU.w() << ", " << R_MARKER_FLU.x() << ", " << R_MARKER_FLU.y() << ", " << R_MARKER_FLU.z());
-      ROS_INFO_STREAM("Magnitude: " << rad2deg(Eigen::AngleAxisd(R_MARKER_FLU).angle()) );
-
-      cout << "yaml:" << endl;
-      cout << "R_MARKER_FLU:\n"
-           << "   x: " << R_MARKER_FLU.x() << '\n'
-           << "   y: " << R_MARKER_FLU.y() << '\n'
-           << "   z: " << R_MARKER_FLU.z() << '\n'
-           << "   w: " << R_MARKER_FLU.w() << '\n' << endl;
-
-    }
-
-};
+}
 
 int main( int argc, char** argv ) {
 
@@ -186,23 +171,11 @@ int main( int argc, char** argv ) {
 
   ros::NodeHandle nh;
 
-  cali_sampler_t cali_sampler;
-
-  ros::Subscriber sub_imu = nh.subscribe(
-          "imu",
-          100,
-          &cali_sampler_t::on_imu,
-          &cali_sampler,
-          ros::TransportHints().tcpNoDelay()
-  );
-
-  ros::Subscriber sub_odom = nh.subscribe(
-          "odom",
-          100,
-          &cali_sampler_t::on_odom,
-          &cali_sampler,
-          ros::TransportHints().tcpNoDelay()
-  );
+  message_filters::Subscriber<sensor_msgs::Imu> sub_imu(nh, "/djiros/imu", 1);
+  message_filters::Subscriber<nav_msgs::Odometry> sub_odom(nh, "/uwb_vicon_odom", 1);
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu,nav_msgs::Odometry> sync_pol;
+  message_filters::Synchronizer<sync_pol> sync(sync_pol(10),sub_imu,sub_odom);
+  sync.registerCallback(sync_callback);
 
   ros::spin();
 
