69ac320de84b15c32f08f6580270d83ae633407d
plutotx <ptx.pluto@gmail.com>
2020-08-31 15:49:57 +0800

    add fsm

--- rnw_ros/src/rnw_planning_node.cpp
+++ rnw_ros/src/rnw_planning_node.cpp
@@ -10,9 +10,62 @@ using namespace std;
 
 struct rnw_planner_t {
 
-    explicit rnw_planner_t( ros::NodeHandle & nh ){}
+    static constexpr double deg2rad = M_PI/180.;
+
+    static constexpr double min_tilt = 20 * deg2rad;
+
+    double ang_vel_threshold = 1;
+
+    enum class cone_fsm_e {
+        idle, qstatic, rocking
+    };
+
+    cone_fsm_e fsm;
+
+    rnw_ros::ConeState latest_cone_state;
+
+    explicit rnw_planner_t( ros::NodeHandle & nh ){
+
+    }
 
     void on_cone_state( rnw_ros::ConeStateConstPtr const & msg ){
+
+      latest_cone_state = *msg;
+
+      update_state(latest_cone_state);
+
+      switch ( fsm ) {
+        case cone_fsm_e::idle:
+          break;
+        case cone_fsm_e::rocking:
+          break;
+        case cone_fsm_e::qstatic:
+          plan_next_position();
+          break;
+        default:
+          ROS_ERROR_STREAM("[rnw_planner] Invalid Cone State");
+          break;
+      }
+
+    }
+
+    void plan_next_position(){
+      Vector3d contact_point = uav_utils::from_point_msg(latest_cone_state.contact_point);
+      Vector3d apex = uav_utils::from_point_msg(latest_cone_state.tip);
+
+    }
+
+    void update_state( rnw_ros::ConeState const & msg ){
+
+      if ( msg.euler_angles.x < min_tilt ) {
+        fsm = cone_fsm_e::idle;
+      }
+      else if ( msg.euler_angles_velocity.y < ang_vel_threshold ) {
+        fsm = cone_fsm_e::qstatic;
+      }
+      else {
+        fsm = cone_fsm_e::rocking;
+      }
 
     }
 
