890f3117359da36157fab65c136981ab4b64dbc2
plutotx <ptx.pluto@gmail.com>
2020-11-18 13:45:45 +0800

    add energy plot

--- matlab_utils/paper_data.m
+++ matlab_utils/paper_data.m
@@ -3,9 +3,15 @@
 
 
 %size(dat,1)
+load('amp25.mat');
 
+plot(1:73,dat);
 
 amps = calc_amps(dat);
+
+amps_deg = rad2deg(amps);
+
+% amps_filtered = filter_outlier(amps_deg,10);
 
 rad2deg(mean(amps))
 rad2deg(std(amps))
@@ -16,5 +22,18 @@ function amps = calc_amps(spins)
     for i = 2:n
         val = abs(spins(i) - spins(i-1))/2;
         amps = [amps,val];
+    end
+    amps = amps';
+end
+
+
+function amps = filter_outlier(src,m)
+    n = size(src,1);
+    amps = [];
+    for i=1:n
+        v = src(i);
+        if v > m
+            amps = [amps,v];
+        end
     end
 end
---
+++ rnw_ros/nodes/pub_energy_node.cpp
@@ -0,0 +1,100 @@
+#include <ros/ros.h>
+
+#include "rnw_ros/rnw_utils.h"
+
+#include <rnw_msgs/WalkingState.h>
+#include <quadrotor_msgs/Float64Stamped.h>
+
+ros::Publisher pub_energy;
+ros::Publisher pub_energy_T;
+ros::Publisher pub_energy_V;
+
+rnw_config_t rnw_config;
+
+Vector3d pt_on_cone( Vector3d const & pt, rnw_msgs::ConeState const & cone_state ){
+  Quaterniond R = uav_utils::from_quaternion_msg(cone_state.odom.pose.pose.orientation);
+  Vector3d T = uav_utils::from_point_msg(cone_state.odom.pose.pose.position);
+  return R * pt + T;
+}
+
+void on_walking_state( rnw_msgs::WalkingStateConstPtr const & msg ){
+
+  double com_z = 0.05;
+  double com_x = 0.08;
+
+  Vector3d CoM = rnw_config.cone.base_center;
+  CoM.z() = CoM.z() + com_z;
+  CoM.x() = CoM.x() + com_x;
+
+  static rnw_msgs::WalkingState pre_msg;
+  static bool init = false;
+
+  if ( !init ) {
+    pre_msg = *msg;
+    init = true;
+    return;
+  }
+
+  rnw_msgs::WalkingState cur_msg = *msg;
+
+  double dt = ( cur_msg.header.stamp - pre_msg.header.stamp ).toSec(); // sec
+
+  Vector3d pt_pre = pt_on_cone(CoM,pre_msg.cone_state);
+  Vector3d pt_cur = pt_on_cone(CoM,cur_msg.cone_state);
+
+  double dist = ( pt_cur - pt_pre ).norm(); // m
+  double v = dist/dt; // m/s
+
+  double m = 0.5; // kg
+  double g = 9.8;
+  double h = pt_cur.z(); // m
+
+  double T = 0.5 * m * v * v;
+  double V = m * g * h;
+
+  double E = T+V;
+
+  quadrotor_msgs::Float64Stamped msg_out;
+  msg_out.header = msg->header;
+
+  msg_out.value = E;
+  pub_energy.publish(msg_out);
+
+  msg_out.value = T;
+  pub_energy_T.publish(msg_out);
+
+  msg_out.value = V;
+  pub_energy_V.publish(msg_out);
+
+  pre_msg = cur_msg;
+
+}
+
+int main( int argc, char** argv ) {
+
+  ros::init(argc,argv,"pub_energy_node");
+
+  ros::NodeHandle nh("~");
+
+  rnw_config.load_from_ros(nh);
+
+  ros::Subscriber sub_imu = nh.subscribe<rnw_msgs::WalkingState>(
+          "/rnw/walking_state/session_1",
+          100,
+          on_walking_state,
+          ros::TransportHints().tcpNoDelay()
+  );
+
+  pub_energy = nh.advertise<quadrotor_msgs::Float64Stamped>("/rnw/cone_energy",100);
+
+  pub_energy_T = nh.advertise<quadrotor_msgs::Float64Stamped>("/rnw/cone_energy_T",100);
+
+  pub_energy_V = nh.advertise<quadrotor_msgs::Float64Stamped>("/rnw/cone_energy_V",100);
+
+  ros::spin();
+
+  ros::shutdown();
+
+  return 0;
+
+}
