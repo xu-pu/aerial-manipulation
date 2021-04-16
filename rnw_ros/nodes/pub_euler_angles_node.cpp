07acbe051cdbd03603276c8a29c3f5455fa13e3a
plutotx <ptx.pluto@gmail.com>
2021-04-16 16:31:32 +0800

    bug fix

--- rnw_ros/nodes/pub_euler_angles_node.cpp
+++ rnw_ros/nodes/pub_euler_angles_node.cpp
@@ -50,7 +50,7 @@ int main( int argc, char** argv ) {
   );
 
   ros::Subscriber sub_odom = nh.subscribe<nav_msgs::Odometry>(
-          "/odom/uav",
+          "odom",
           10,
           on_odom,
           ros::TransportHints().tcpNoDelay()
