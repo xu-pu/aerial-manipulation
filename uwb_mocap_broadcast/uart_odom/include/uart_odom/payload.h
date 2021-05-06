e520b3ded3a7dd50985b91c72539137074b11e4d
plutotx <ptx.pluto@gmail.com>
2021-05-06 22:18:07 +0800

    refactoring

--- uwb_mocap_broadcast/uart_odom/include/uart_odom/payload.h
+++ uwb_mocap_broadcast/uart_odom/include/uart_odom/payload.h
@@ -11,36 +11,49 @@ namespace uwb_comm {
 
     struct payload_base_t {
 
+        //========== Master Node ============
+
         /**
-         * Register ros::Subscriber here
+         * Register ros::Publisher and ros::Subscriber here
          */
         virtual void init_as_master() = 0;
 
-        /**
-         * Register ros::Publisher here
-         */
-        virtual void init_as_slave() = 0;
+        virtual int data_length_master() = 0;
 
         /**
-         * Encode and write data frame to the buffer (as master)
+         * Encode data to be sent from master
          * @param buffer
          * @return if true, send the data, if false, don't send anything
          */
         virtual bool master_encode(char * buffer) = 0;
 
         /**
-         * Decode data in the buffer and send it out (as slave)
+         * Decode data sent from slave
+         * @param buffer
+         */
+        virtual void master_decode(char const * buffer) = 0;
+
+        //========== Slave Node ============
+
+        /**
+         * Register ros::Publisher and ros::Subscriber here
+         */
+        virtual void init_as_slave() = 0;
+
+        virtual int data_length_slave() = 0;
+
+        /**
+         * Encode data to be sent from slave
+         * @param buffer
+         * @return if true, send the data, if false, don't send anything
+         */
+        virtual bool slave_encode(char * buffer) = 0;
+
+        /**
+         * Decode data sent from master
          * @param buffer
          */
         virtual void slave_decode(char const * buffer) = 0;
-
-        virtual bool slave_encode(char * buffer) = 0;
-
-        virtual void master_decode(char const * buffer) = 0;
-
-        virtual int data_length_master() = 0;
-
-        virtual int data_length_slave() = 0;
 
     };
 
