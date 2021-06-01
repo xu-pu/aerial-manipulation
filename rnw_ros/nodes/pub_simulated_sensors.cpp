ce4f3ab960fa9dc526c6cd17acef896655ebe5db
plutotx <ptx.pluto@gmail.com>
2021-06-01 22:35:23 +0800

    new sensor sim node worked

--- rnw_ros/nodes/pub_simulated_sensors.cpp
+++ rnw_ros/nodes/pub_simulated_sensors.cpp
@@ -7,6 +7,51 @@
 #include <rnw_ros/ros_utils.h>
 
 
+struct vel_acc_estimator_t {
+
+    Vector3d vel;
+
+    Vector3d acc;
+
+    void update( Vector3d const & p, ros::Time const t ){
+
+      p2 = p1;
+      p1 = p0;
+      p0 = p;
+
+      t2 = t1;
+      t1 = t0;
+      t0 = t;
+
+      if ( counter > 3 ) {
+        vel = (p0-p1)/(t0-t1).toSec();
+        Vector3d vel_pre = (p1-p2)/(t1-t2).toSec();
+        acc = (vel - vel_pre)/(t0-t1).toSec();
+      }
+      else {
+        vel = Vector3d::Zero();
+        acc = Vector3d::Zero();
+      }
+
+      counter++;
+
+    }
+
+private:
+
+    Vector3d p0;
+    Vector3d p1;
+    Vector3d p2;
+
+    ros::Time t0;
+    ros::Time t1;
+    ros::Time t2;
+
+    size_t counter = 0;
+
+};
+
+
 Eigen::Vector3d gps2pos( sensor_msgs::NavSatFix const & msg ){
 
   static bool init = false;
@@ -46,9 +91,9 @@ struct sensor_sim_t {
 
     sensor_msgs::NavSatFix latest_gps;
 
-    geometry_msgs::Vector3 latest_acc;
+    vel_acc_estimator_t vel_acc;
 
-    //gps_estimator_t gps;
+    Vector3d g = Vector3d(0,0,9.81);
 
     sensor_sim_t(){
 
@@ -82,19 +127,26 @@ struct sensor_sim_t {
      */
     void on_imu( sensor_msgs::ImuConstPtr const & msg ){
       latest_imu = *msg;
-      latest_imu.linear_acceleration = latest_acc;
+      latest_imu.linear_acceleration = uav_utils::to_vector3_msg(
+              uav_utils::from_quaternion_msg(latest_imu.orientation).inverse() * (vel_acc.acc+g)
+      );
       pub_imu.publish(latest_imu);
     }
 
     void on_gps( sensor_msgs::NavSatFixConstPtr const & msg ){
       latest_gps = *msg;
       Vector3d pos = gps2pos(*msg);
+      vel_acc.update(pos,latest_gps.header.stamp);
+
       nav_msgs::Odometry odom;
       odom.header.stamp = msg->header.stamp;
       odom.header.frame_id = "world";
       odom.pose.pose.orientation = latest_imu.orientation;
       odom.pose.pose.position = uav_utils::to_point_msg(pos);
+      odom.twist.twist.angular = latest_imu.angular_velocity;
+      odom.twist.twist.linear = uav_utils::to_vector3_msg(vel_acc.vel);
       pub_odom.publish(odom);
+
     }
 
 };
