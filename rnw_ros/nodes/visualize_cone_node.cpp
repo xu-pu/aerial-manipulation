39a376be2eeb3bcb92ed44d08638ade49638a06e
plutotx <ptx.pluto@gmail.com>
2021-06-13 03:17:52 +0800

    remove garbage

--- rnw_ros/nodes/visualize_cone_node.cpp
+++ rnw_ros/nodes/visualize_cone_node.cpp
@@ -1,5 +1,5 @@
 #include "rnw_ros/pose_utils.h"
-#include "rnw_ros/rnw_planner.h"
+#include "rnw_ros/rnw_config.h"
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <rnw_msgs/ConeState.h>
