4dbddfd6dad2d6e9362055e813de12922b476658
plutotx <ptx.pluto@gmail.com>
2021-06-28 03:09:06 +0800

    extract thrust data

---
+++ rnw_ros/scripts/extract_thrust.cpp
@@ -0,0 +1,53 @@
+//
+// Created by sheep on 2021/6/28.
+//
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/Joy.h>
+#include <fstream>
+
+using namespace std;
+
+
+int main( int argc, char** argv ){
+
+  std::string bag_dir = "/home/sheep/Dropbox/mphil_bags";
+  std::string bag_name = "2021-06-09-00-18-07.perfect2.drone1.steel.65.120.bag";
+  std::string result_dir = "/home/sheep/";
+
+  rosbag::Bag bag;
+  {
+    stringstream ss; ss << bag_dir << "/" << bag_name;
+    bag.open(ss.str());
+  }
+
+  stringstream ss_out;
+  ss_out << result_dir << "/" << bag_name << ".thrust.csv";
+  ofstream ofs(ss_out.str());
+
+  std::vector<std::string> topics;
+  topics.emplace_back("/djiros/ctrl");
+  rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+  bool init = false;
+  ros::Time init_time;
+
+  for (rosbag::MessageInstance const m: view) {
+    sensor_msgs::Joy::ConstPtr i = m.instantiate<sensor_msgs::Joy>();
+    if (i == nullptr) continue;
+    if ( !init ) {
+      init = true;
+      init_time = i->header.stamp;
+    }
+
+    double t = (i->header.stamp - init_time).toSec();
+    double thrust = i->axes.at(2);
+
+    ofs << t << "," << thrust << endl;
+
+  }
+
+  ofs.close();
+  bag.close();
+
+}
