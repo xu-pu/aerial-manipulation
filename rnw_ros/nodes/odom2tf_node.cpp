02794a3084eceeb386afb0014c372b20f1b394b3
plutotx <ptx.pluto@gmail.com>
2021-08-26 08:57:28 +0800

    pose2tf added

--- rnw_ros/nodes/odom2tf_node.cpp
+++ rnw_ros/nodes/odom2tf_node.cpp
@@ -5,8 +5,16 @@
 
 string tf_frame_name, tf_parent_name;
 
+void pose2tf( geometry_msgs::Pose const & pose, string const & name, string const & parent ){
+  publish_frame( pose2R(pose), pose2T(pose), name, parent );
+}
+
 void on_odom( nav_msgs::OdometryConstPtr const & msg ){
-  publish_frame(odom2R(msg),odom2T(msg),tf_frame_name,tf_parent_name);
+  pose2tf(msg->pose.pose,tf_frame_name,tf_parent_name);
+}
+
+void on_pose( geometry_msgs::PoseStampedConstPtr const & msg ){
+  pose2tf(msg->pose,tf_frame_name,tf_parent_name);
 }
 
 int main( int argc, char** argv ) {
@@ -18,7 +26,18 @@ int main( int argc, char** argv ) {
   tf_frame_name = get_param_default<string>(nh,"name","name");
   tf_parent_name = get_param_default<string>(nh,"parent","world");
 
-  ros::Subscriber sub_odom = nh.subscribe<nav_msgs::Odometry>("odom",1,on_odom);
+  ros::Subscriber sub_odom = nh.subscribe<nav_msgs::Odometry>(
+          "odom",
+          1,
+          on_odom,
+          ros::TransportHints().tcpNoDelay()
+  );
+
+  ros::Subscriber sub_pose = nh.subscribe<geometry_msgs::PoseStamped>(
+          "pose",
+          1,on_pose,
+          ros::TransportHints().tcpNoDelay()
+  );
 
   ros::spin();
 
